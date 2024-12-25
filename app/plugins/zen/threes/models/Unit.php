51a21075298abe982f6023bc50da34f1fbb2b479
Alex Blaze <zen@8ber.ru>
2024-12-25 15:39:23 +0400

    Some fixes

--- app/plugins/zen/threes/models/Unit.php
+++ app/plugins/zen/threes/models/Unit.php
@@ -8,7 +8,9 @@ class Unit extends Model
     use Validation;
 
     public $table = 'zen_threes_units';
-    public $rules = [];
+    public $rules = [
+        'tid' => 'unique:zen_threes_units,tid',
+    ];
     protected $primaryKey = 'tid';
     public $incrementing = false;
     protected $keyType = 'string';
@@ -25,11 +27,20 @@ class Unit extends Model
 
     public function beforeCreate()
     {
+        $tid = $this->tid ?? $this->attributes['tid'] ?? null;
 
-
-
-        if (!$this->tid && !isset($this->attributes['tid'])) {
+        if (!$tid) {
             $this->attributes['tid'] = \Str::uuid();
         }
+    }
+
+    public function getTidAttribute($value)
+    {
+        if ($value) {
+            return $value;
+        }
+
+        $author_token = ths()->settings('author_token') ?? 'project';
+        return $author_token . '.';
     }
 }
