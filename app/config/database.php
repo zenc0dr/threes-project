80ca384b04e0736b94ecb4ee40f5b9aa9b517276
Alex Blaze <zen@8ber.ru>
2025-05-01 21:34:30 +0400

    Настройки DB

--- app/config/database.php
+++ app/config/database.php
@@ -102,6 +102,19 @@ return [
             'prefix_indexes' => true,
         ],
 
+        # Настройки для mongoDB
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('MONGO_DB_HOST', '127.0.0.1'),
+            'port'     => env('MONGO_DB_PORT', 27017),
+            'database' => env('MONGO_DB_DATABASE'),
+            'username' => env('MONGO_DB_USERNAME'),
+            'password' => env('MONGO_DB_PASSWORD'),
+            'options'  => [
+                'authSource' => 'admin'
+            ],
+        ],
+
     ],
 
     /*
