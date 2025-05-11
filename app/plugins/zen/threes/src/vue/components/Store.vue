e3421fcf974f5b7a11893c35fceddff383e1f869
Alex Blaze <zen@8ber.ru>
2025-05-11 13:09:36 +0400

    Some fixes

--- app/plugins/zen/threes/src/vue/components/Store.vue
+++ app/plugins/zen/threes/src/vue/components/Store.vue
@@ -1,20 +1,27 @@
 <template>
-<div class="threes-store">
-    <div @click="addNode" v-for="node in nodes" class="threes-store__node">
-        <div class="threes-store__head">
-            <div class="threes-store__icon">
-                <icon :src="node.icon" width="20px" height="20px" />
-            </div>
-            <div class="threes-store__name">
-                {{ node.name }}
-            </div>
+    <div class="threes-store">
+        <div class="threes-store__header">
+            <div class="col col-nid">nid</div>
+            <div class="col col-icon">icon</div>
+            <div class="col col-name">name</div>
+            <div class="col col-group">group</div>
         </div>
-        <div class="threes-store__description">
-            {{ node.description }}
+        <div
+            class="threes-store__row"
+            v-for="node in nodes"
+            :key="node.nid"
+            @click="addNode(node)"
+        >
+            <div class="col col-nid">{{ node.nid || 'template' }}</div>
+            <div class="col col-icon">
+                <icon :src="node.icon" width="24px" height="24px" />
+            </div>
+            <div class="col col-name">{{ node.name }}</div>
+            <div class="col col-group">{{ node.group }}</div>
         </div>
     </div>
-</div>
 </template>
+
 <script>
 import icon from './icon.vue'
 export default {
@@ -25,7 +32,7 @@ export default {
     data() {
         return {
             ths: window.ths,
-            nodes: null
+            nodes: []
         }
     },
     mounted() {
@@ -44,55 +51,56 @@ export default {
                 }
             })
         },
-        addNode() {
-            console.log(ths.data.selected_nid)
+        addNode(node) {
+            console.log('Add node', node.nid || 'template')
         }
     }
 }
 </script>
+
 <style lang="scss">
 .threes-store {
     display: flex;
+    flex-direction: column;
     background: #a7a7a7;
-    padding: 15px;
-    flex-wrap: wrap;
+    padding: 10px;
     border-top: 2px solid #ffe097;
 
-    &__node {
+    &__header, &__row {
         display: flex;
-        flex-direction: column;
-        width: 240px;
-        background: #f3f3f3;
-        border-radius: 5px;
-        margin: 3px;
-    }
-    &__head {
-        display: flex;
-        flex-direction: row;
         align-items: center;
-        justify-content: flex-start;
-        padding: 10px 10px 8px;
-        border-bottom: 1px solid #e7e7e7;
+        padding: 6px 10px;
         background: #fff;
         border-radius: 4px;
+        margin-bottom: 4px;
     }
-    &__icon {
-        width: 20px;
-        height: 20px;
-        margin-right: 6px;
-    }
-    &__name {
-        padding: 10px;
-        line-height: 16px;
-        font-size: 15px;
+
+    &__header {
         font-weight: bold;
+        background: #ececec;
     }
-    &__description {
-        padding: 10px;
-        font-size: 12px;
-        max-height: 110px;
-        overflow-y: auto;
-        color: #424242;
+
+    .col {
+        flex: 0 0 auto; // <-- фиксирует ширину (grow: 0, shrink: 0, auto basis)
+        padding: 4px 8px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+
+        &.col-nid {
+            width: 120px;
+        }
+        &.col-icon {
+            width: 40px;
+        }
+        &.col-group {
+            width: 140px;
+        }
+        &.col-name {
+            flex: 1 1 auto; // <-- тянется
+            min-width: 0;
+        }
     }
+
 }
 </style>
