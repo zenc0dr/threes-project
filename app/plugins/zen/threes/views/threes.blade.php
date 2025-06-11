81d2f0796d8ace7517dd7d63bfe5b40ff2a85256
Alex Blaze <zen@8ber.ru>
2025-06-11 14:23:14 +0400

    Some fixes

--- app/plugins/zen/threes/views/threes.blade.php
+++ app/plugins/zen/threes/views/threes.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+    <title>Threes</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Threes</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/svg+xml" href="/plugins/zen/threes/assets/images/icons/threes.svg">
     <link href="/modules/backend/assets/vendor/bootstrap/bootstrap.css" rel="stylesheet">
     <link href="/modules/backend/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
     <link href="/modules/backend/assets/css/october.css" rel="stylesheet">
