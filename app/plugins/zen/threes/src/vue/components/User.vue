3841b8af361bfc1b1bbcb319ff41c1282c4f55e8
Alex Blaze <zen@8ber.ru>
2025-07-01 00:29:57 +0400

    Some fixes

--- app/plugins/zen/threes/src/vue/components/User.vue
+++ app/plugins/zen/threes/src/vue/components/User.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="threes-user">
+    <div class="threes-user" :class="{ mobile: isMobile }">
         <div class="tree-user-info">
+            <!-- Мобильная кнопка меню -->
+            <div v-if="isMobile" class="mobile-menu-btn" @click="$emit('toggle-mobile-menu')">
+                <i class="oc-icon-bars" title="Открыть меню"></i>
+            </div>
+            
             <div v-if="user" class="user-name" @click="$router.push('/profile')">
                 {{ user.name || user.login }}
             </div>
@@ -19,6 +24,7 @@ export default {
         return {
             ths: window.ths,
             user: null,
+            isMobile: false
         };
     },
     watch: {
@@ -30,21 +36,24 @@ export default {
     },
     mounted() {
         this.getUser()
+        this.checkMobile()
+        window.addEventListener('resize', this.checkMobile)
+    },
+    beforeUnmount() {
+        window.removeEventListener('resize', this.checkMobile)
     },
     methods: {
+        checkMobile() {
+            this.isMobile = ths.isMobile()
+        },
         getUser() {
             if (this.ths && this.ths.data && this.ths.data.user) {
                 this.user = this.ths.data.user
             } else {
                 // Если пользователь еще не загружен, ждем немного
                 setTimeout(() => {
-                    this.checkUser()
+                    this.getUser()
                 }, 1000)
-            }
-        },
-        checkUser() {
-            if (this.ths && this.ths.data && this.ths.data.user) {
-                this.user = this.ths.data.user
             }
         },
         logout() {
@@ -60,6 +69,16 @@ export default {
     background: #f8f9fa;
     border-top: 1px solid #e9ecef;
 
+    &.mobile {
+        border-top: none;
+        background: #ffffff;
+        
+        .tree-user-info {
+            padding: 8px 16px;
+            min-height: 50px;
+        }
+    }
+
     .tree-user-info {
         padding: 12px 16px;
         display: flex;
@@ -68,6 +87,24 @@ export default {
         gap: 12px;
         min-height: 60px;
         box-sizing: border-box;
+
+        .mobile-menu-btn {
+            flex-shrink: 0;
+            
+            i {
+                padding: 8px;
+                background: #f7f7f7;
+                color: #666;
+                border-radius: 4px;
+                cursor: pointer;
+                transition: 200ms;
+                font-size: 16px;
+                
+                &:hover {
+                    background: #e7e7e7;
+                }
+            }
+        }
 
         .user-name {
             color: #495057;
